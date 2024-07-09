d97009fbb3f8701e83b3517634d80ef2658552bb
FantomStudy <arzamastsevdaniil@gmail.com>
2024-07-10 01:08:11 +0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,19 @@ import "./App.css";
 import Header from "./components/header/Header";
 import HelloPage from "./pages/helloPage/HelloPage";
 import AboutPage from "./pages/aboutPage/AboutPage";
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/CampSite/" element={<HelloPage />} />
           <Route path="/CampSite/about" element={<AboutPage />} />
           <Route path="/CampSite/news" element={<AboutPage />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }

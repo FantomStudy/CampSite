4b0b20a14465b80586bfa7c73433496c25f0062c
FantomStudy <arzamastsevdaniil@gmail.com>
2024-07-09 12:52:36 +0500

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./components/header/Header";
 import HelloPage from "./pages/helloPage/HelloPage";
 import AboutPage from "./pages/aboutPage/AboutPage";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Footer from "./components/footer/Footer";
 
 export default function App() {
   return (
